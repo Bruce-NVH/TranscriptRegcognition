8ff92cdc515fd76050d94061dff9bf6ac75fdd2e
bruce-h <hunghonny@gmail.com>
2023-02-03 16:54:34 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 import numpy as np
 import os
 
@@ -6,7 +7,7 @@ import os
 # Cac duong mau vang la khung phat hien vung bang de cat anh
 
 working_directory = os.getcwd()
-IMAGE = cv2.imread(working_directory + "\Images\Anh1.jpg")
+IMAGE = cv2.imread(working_directory + "\Images\Anh5.jpg")
 detectedLinesImg = IMAGE.copy()
 
 
@@ -18,9 +19,9 @@ def sortY(val):
     return val[1]
 
 
-def preprocess(image):
+def preprocess(image, level):
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Chuyen ve anh xam
-    smooth_img = cv2.GaussianBlur(gray_img, (5, 5), 0)  # Lam min anh
+    smooth_img = cv2.GaussianBlur(gray_img, (level, level), 0)  # Lam min anh
     im_bw = cv2.adaptiveThreshold(smooth_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15,
                                   8)  # Nhi phan anh
     return im_bw
@@ -29,7 +30,7 @@ def preprocess(image):
 def findHorAndVerLines(image, rawImg):
     h_image, w_image, c_image = rawImg.shape
 
-    edges = cv2.Canny(image, 100, 200, None, 3)  # Phat hien canh de xu ly Hough
+    edges = cv2.Canny(image, 100, 200, None, 3)  # Phat hien canh de xu ly Hough]
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 250, None, None, h_image)
 
     horizontal_lines_tmp = []
@@ -44,11 +45,11 @@ def findHorAndVerLines(image, rawImg):
     if lines is not None:
         for i in range(0, len(lines)):
             l = lines[i][0]
-            if abs(l[3] - l[1]) < h_image / 60 and abs(
+            if abs(l[3] - l[1]) < h_image / 30 and abs(
                     l[2] - l[0]) > w_image * 2 / 3:  # Neu la duong ngang thi them vao
                 sum_avg_horizontal_line += abs(l[2] - l[0])
                 horizontal_lines_tmp.append(l)
-            if abs(l[2] - l[0]) < w_image / 40:  # Neu la duong doc thi them vao
+            if abs(l[2] - l[0]) < w_image / 20:  # Neu la duong doc thi them vao
                 sum_avg_vertical_line += abs(l[3] - l[1])
                 vertical_lines_tmp.append(l)
 
@@ -63,7 +64,7 @@ def findHorAndVerLines(image, rawImg):
         avg_vertical_line = sum_avg_vertical_line / len(vertical_lines_tmp)
         for i in range(0, len(vertical_lines_tmp)):
             l = vertical_lines_tmp[i]
-            if abs(l[3] - l[1]) >= avg_vertical_line * 0.99:
+            if abs(l[3] - l[1]) >= avg_vertical_line * 0.75:
                 vertical_lines.append(l)
 
         # Sap xep lai cac duong thang theo thu tu tu trai sang phai
@@ -94,7 +95,7 @@ def findHorAndVerLines(image, rawImg):
             avg_vertical_line = sum_avg_vertical_line / len(vertical_lines_tmp)
             for i in range(0, len(vertical_lines_tmp)):
                 l = vertical_lines_tmp[i]
-                if abs(l[3] - l[1]) >= avg_vertical_line * 0.99:
+                if abs(l[3] - l[1]) >= avg_vertical_line * 0.75:
                     vertical_lines.append(l)
 
         # Sap xep lai cac duong thang theo thu tu tu tren xuong duoi
@@ -116,18 +117,6 @@ def findHorAndVerLines(image, rawImg):
                 l[3] = l1
 
         return horizontal_lines, vertical_lines
-
-
-im_bw = preprocess(IMAGE)
-(horizontal_lines, vertical_lines) = findHorAndVerLines(im_bw, IMAGE)
-
-# Ve cac duong thang ngang va doc
-for i in range(0, len(horizontal_lines)):
-    l = horizontal_lines[i]
-    cv2.line(detectedLinesImg, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
-for i in range(0, len(vertical_lines)):
-    l = vertical_lines[i]
-    cv2.line(detectedLinesImg, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
 
 
 def findCropPoint(image, horizontalLines, verticalLines):
@@ -159,10 +148,10 @@ def findCropPoint(image, horizontalLines, verticalLines):
         x1 = int((crop_vertical_lines[0][0] + crop_vertical_lines[0][2]) / 2)
         x2 = int((crop_vertical_lines[-1][0] + crop_vertical_lines[-1][2]) / 2)
 
-        cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 3, cv2.LINE_AA)
-        cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
-        cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 3, cv2.LINE_AA)
-        cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
+        cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 2, cv2.LINE_AA)
+        cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
+        cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 2, cv2.LINE_AA)
+        cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
 
         return x1, x2, y1, y2
 
@@ -224,10 +213,10 @@ def findCropPoint(image, horizontalLines, verticalLines):
                     y1 = ver_line_avg_pos_top
                     y2 = int((l2[1] + l2[3]) / 2)
 
-                cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 3, cv2.LINE_AA)
-                cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
-                cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 3, cv2.LINE_AA)
-                cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
+                cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 2, cv2.LINE_AA)
+                cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
+                cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 2, cv2.LINE_AA)
+                cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
 
                 return x1, x2, y1, y2
             else:
@@ -240,15 +229,53 @@ def findCropPoint(image, horizontalLines, verticalLines):
             y1 = int((l1[1] + l1[3]) / 2)
             y2 = int((l2[1] + l2[3]) / 2)
 
-            cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 3, cv2.LINE_AA)
-            cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
-            cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 3, cv2.LINE_AA)
-            cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 3, cv2.LINE_AA)
+            cv2.line(image, (x1, y1), (x2, y1), (25, 183, 227), 2, cv2.LINE_AA)
+            cv2.line(image, (x1, y2), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
+            cv2.line(image, (x1, y1), (x1, y2), (25, 183, 227), 2, cv2.LINE_AA)
+            cv2.line(image, (x2, y1), (x2, y2), (25, 183, 227), 2, cv2.LINE_AA)
 
             return x1, x2, y1, y2
 
+def extendCropImg(img, x_1, x_2, y_1, y_2):
+    h_image, w_image, c_image = img.shape
 
-x1, x2, y1, y2 = findCropPoint(detectedLinesImg, horizontal_lines, vertical_lines)
+    if x_1 - 5 > 0:
+        x_1 -= 5
+    else:
+        x_1 = 0
+
+    if x_2 + 5 < w_image:
+        x_2 += 5
+    else:
+        x_2 = w_image
+
+    if y_1 - 5 > 0:
+        y_1 -= 5
+    else:
+        y_1 = 0
+
+    if y_2 + 5 < h_image:
+        y_2 += 5
+    else:
+        y_2 = h_image
+
+    return x_1, x_2, y_1, y_2
+
+
+im_bw = preprocess(IMAGE, 5)
+(horizontal_lines, vertical_lines) = findHorAndVerLines(im_bw, IMAGE)
+
+# Ve cac duong thang ngang va doc
+for i in range(0, len(horizontal_lines)):
+    l = horizontal_lines[i]
+    cv2.line(detectedLinesImg, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 2, cv2.LINE_AA)
+for i in range(0, len(vertical_lines)):
+    l = vertical_lines[i]
+    cv2.line(detectedLinesImg, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 2, cv2.LINE_AA)
+
+_x1, _x2, _y1, _y2 = findCropPoint(detectedLinesImg, horizontal_lines, vertical_lines)
+x1, x2, y1, y2 = extendCropImg(IMAGE, _x1, _x2, _y1, _y2)
+
 
 cv2.imshow('detectedLines', cv2.resize(detectedLinesImg, (480, 640)))
 img_crop = IMAGE[y1:y2, x1:x2]
@@ -259,16 +286,28 @@ cv2.waitKey()
 # ####################### NHAN DIEN CAC DUONG THANG DOC###############################
 #
 
-img_crop_bw = preprocess(img_crop)
+img_crop_bw = preprocess(img_crop, 5)
+#
+# vertical = numpy.copy(img_crop_bw)
+# rows = vertical.shape[0]
+# verticalSize = rows // 700
+# verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (4, verticalSize))
+# vertical = cv2.erode(vertical, verticalStructure)
+# vertical = cv2.dilate(vertical, verticalStructure)
+# cv2.imshow('vertical', cv2.resize(vertical, (480, 640)))
+
 imgCropHorLines, imgCropVerLines = findHorAndVerLines(img_crop_bw, img_crop)
 detectedLinesImgCrop = img_crop.copy()
 
-for i in range(0, len(imgCropHorLines)):
-    l = imgCropHorLines[i]
-    cv2.line(detectedLinesImgCrop, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+
+
+# for i in range(0, len(imgCropHorLines)):
+#     l = imgCropHorLines[i]
+#     cv2.line(detectedLinesImgCrop, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 2, cv2.LINE_AA)
 for i in range(0, len(imgCropVerLines)):
     l = imgCropVerLines[i]
-    cv2.line(detectedLinesImgCrop, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.line(detectedLinesImgCrop, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 2, cv2.LINE_AA)
 
+# cv2.imshow('imbw', cv2.resize(img_crop_bw, (480, 640)))
 cv2.imshow('detectedLinesImgCrop', cv2.resize(detectedLinesImgCrop, (480, 640)))
 cv2.waitKey()
